85fd0e71d9bef250dc529a01303fafdcfaf0703a
Antoine P <depertat@hotmail.fr>
2021-09-14 16:46:44 +0200

    adding Sidebar with style

---
+++ twitter-clone/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
+
+function App() {
+  return (
+    <div className="app">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
